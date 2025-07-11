02f032a498c16deb01e44fa7bd4b6d87cbf7604c
stepheng87 <scooba75@gmail.com>
2025-07-10 20:38:24 -0500

    created models within app with fields

--- pizzas/models.py
+++ pizzas/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Pizza(models.Model):
+    pizza_name = ["Supreme","Veggie","Meat Lovers","Hawaiian","Cheese"]
+
+
+
+class Topping(models.Model):
+    pizza = ["Supreme","Veggie","Meat Lovers","Hawaiian","Cheese"]
+    topping_name = ["Pepperoni","Sausage","Hamburger","Spinach","Olives","Pineapple","Canadian Bacon","3 Cheese Blend","Onion","Bacon"]
